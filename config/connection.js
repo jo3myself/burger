fddfbeed07f6a24eacfcc0554e8f2086c2c9df4b
CODING <CODING>
2017-09-22 21:50:57 -0700

    adding files

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // set mysql connection
 var mysql=require("mysql");
+var connection;
 
-var connection=mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "aaa",
-  database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(pro.env.JAWSDB_URL);
+} else {
+	connection=mysql.createConnection({
+	  host: "localhost",
+	  port: 3306,
+	  user: "root",
+	  password: "aaa",
+	  database: "burgers_db"
 });
- 
+};
+
 connection.connect(function(err) {
   if (err) throw err;
 }); 
